20937a00b72f5c7f00535d29de6e34acc7f3be9a
Olena Bukhova <lena.bukhova2005@gmail.com>
2025-03-17 21:30:36 +0100

    Made correct links for sidebar and for item-detail

--- taxi/models.py
+++ taxi/models.py
@@ -14,7 +14,7 @@ class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
-        return self.username
+        return f"{self.first_name} {self.last_name}"
 
 
 class Car(models.Model):
